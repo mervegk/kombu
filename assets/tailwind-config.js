3ba9953f56b01bf26c630f8cb33f5c2933c929d3
mervegk <gekemeke123@gmail.com>
2025-12-02 00:25:13 +0300

    initial commit

---
+++ assets/tailwind-config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ede7ae'
+      },
+      fontFamily: {
+        'nanum': ['Nanum Myeongjo', 'serif'],
+        'cormorant': ['Cormorant', 'serif'],
+        'funnel': ['Funnel Sans', 'sans-serif']
+      }
+    }
+  }
+};
